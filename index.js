d0d9cfaa3f01cee2ac6f543e58a7e3fc2cba961e
Gabriel Sappio <gabriel.sappio02@gmail.com>
2022-09-27 22:02:07 -0300

    problems, fix later

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ let server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify({
             users:[{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
+                name: 'Gabriel',
+                email: 'Gabriel.teste@gmail.com',
                 id: 1
             }]
         }));
